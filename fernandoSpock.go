8581e58baf45a05494de9e91ac6fcfd005f6c6e0
Fernando Luiz Goulart <fernando@leapi.com>
2017-11-07 02:56:08 -0200

    Species printing

--- fernandoSpock.go
+++ fernandoSpock.go
@@ -1,14 +1,20 @@
-package fernandoSpock
+package main
 
 import (
-"encoding/json"
-"fmt"
-"os"
-"net/http"
-"net/url"
-"log"
-"bytes"
+	"encoding/json"
+	"fmt"
+	"os"
+	"net/http"
+	"net/url"
+	"log"
+	"bytes"
 )
+
+// URLs
+var characterSearchUrl = "http://stapi.co/api/v1/rest/character/search"
+var characterUrl = "http://stapi.co/api/v1/rest/character?uid="
+
+// Structs
 
 type CharacterStruct struct {
 	UID                    string  `json:"uid"`
@@ -34,17 +40,16 @@ type CharacterStruct struct {
 	FictionalCharacter     bool    `json:"fictionalCharacter"`
 	Mirror                 bool    `json:"mirror"`
 	AlternateReality       bool    `json:"alternateReality"`
-
 }
 
 type PageJson struct {
-	PageNumber      int64 `json:"pageNumber"`
-	PageSize        int64 `json:"pageSize"`
+	PageNumber       int64 `json:"pageNumber"`
+	PageSize         int64 `json:"pageSize"`
 	NumberOfElements int64 `json:"numberOfElements"`
-	TotalElements   int64 `json:"totalElements"`
-	TotalPages      int64 `json:"totalPages"`
-	FirstPage       bool  `json:"firstPage"`
-	LastPage        bool  `json:"lastPage"`
+	TotalElements    int64 `json:"totalElements"`
+	TotalPages       int64 `json:"totalPages"`
+	FirstPage        bool  `json:"firstPage"`
+	LastPage         bool  `json:"lastPage"`
 }
 
 type SortStruct struct {
@@ -57,57 +62,142 @@ type PageStruct struct {
 	Characters []CharacterStruct `json:"characters"`
 }
 
+type PerformerStruct struct {
+	UID                string `json:"uid"`
+	Name               string `json:"name"`
+	BirthName          string `json:"birthName"`
+	Gender             string `json:"gender"`
+	DateOfBirth        string `json:"dateOfBirth"`
+	PlaceOfBirth       string `json:"placeOfBirth"`
+	DateOfDeath        string `json:"dateOfDeath"`
+	PlaceOfDeath       string `json:"placeOfDeath"`
+	AnimalPerformer    bool   `json:"animalPerformer"`
+	DisPerformer       bool   `json:"disPerformer"`
+	Ds9Performer       bool   `json:"ds9Performer"`
+	EntPerformer       bool   `json:"entPerformer"`
+	FilmPerformer      bool   `json:"filmPerformer"`
+	StandInPerformer   bool   `json:"standInPerformer"`
+	StuntPerformer     bool   `json:"stuntPerformer"`
+	TasPerformer       bool   `json:"tasPerformer"`
+	TngPerformer       bool   `json:"tngPerformer"`
+	TosPerformer       bool   `json:"tosPerformer"`
+	VideoGamePerformer bool   `json:"videoGamePerformer"`
+	VoicePerformer     bool   `json:"voicePerformer"`
+	VoyPerformer       bool   `json:"voyPerformer"`
+}
 
-var characterSearchUrl = "http://stapi.co/api/v1/rest/character/search"
-//TODO: Uncomment characterUrl
-//var characterUrl  = "http://stapi.co/api/v1/rest/character?uid="
-var specieUrl = "http://stapi.co/api/v1/rest/species?uid="
+type SeasonStruct struct {
+	UID   string `json:"uid"`
+	Title string `json:"title"`
+}
 
+type SeriesStruct struct {
+	UID   string `json:"uid"`
+	Title string `json:"title"`
+}
 
-type LocationStruct struct {
-	UID string `json:"uid"`
+type EpisodeStruct struct {
+	UID                    string       `json:"uid"`
+	Title                  string       `json:"title"`
+	TitleGerman            string       `json:"titleGerman"`
+	TitleItalian           string       `json:"titleItalian"`
+	TitleJapanese          string       `json:"titleJapanese"`
+	Series                 SeriesStruct `json:"series"`
+	Season                 SeasonStruct `json:"season"`
+	SeasonNumber           int64        `json:"seasonNumber"`
+	EpisodeNumber          int64        `json:"episodeNumber"`
+	ProductionSerialNumber string       `json:"productionSerialNumber"`
+	FeatureLength          bool         `json:"featureLength"`
+	StardateFrom           float64      `json:"stardateFrom"`
+	StarDateTo             float64      `json:"starDateTo"`
+	YearFrom               int64        `json:"yearFrom"`
+	YearTo                 int64        `json:"yearTo"`
+	UsAirDate              string       `json:"usAirDate"`
+	FinalScriptDate        string       `json:"finalScriptDate"`
+}
+
+type MainDirectorStruct struct {
+	UID  string `json:"uid"`
 	Name string `json:"name"`
 }
 
-type HomeWorldStruct struct {
-	UID string `json:"uid"`
-	Name string `json:"name"`
-	AstronomicalObjectType string `json:"astronomicalObjectType"`
-	Location LocationStruct `json:"location"`
+type MoviesStruct struct {
+	UID                     string             `json:"uid"`
+	Title                   string             `json:"title"`
+	MainDirector            MainDirectorStruct `json:"mainDirector"`
+	TitleBulgarian          string             `json:"titleBulgarian"`
+	TitleCatalan            string             `json:"titleCatalan"`
+	TitleChineseTraditional string             `json:"titleChineseTraditional"`
+	TitleGerman             string             `json:"titleGerman"`
+	TitleItalian            string             `json:"titleItalian"`
+	TitleJapanese           string             `json:"titleJapanese"`
+	TitlePolish             string             `json:"titlePolish"`
+	TitleRussian            string             `json:"titleRussian"`
+	TitleSerbian            string             `json:"titleSerbian"`
+	TitleSpanish            string             `json:"titleSpanish"`
+	StardateFrom            float64            `json:"stardateFrom"`
+	StarDateTo              float64            `json:"starDateTo"`
+	YearFrom                int64              `json:"yearFrom"`
+	YearTo                  int64              `json:"yearTo"`
+	UsReleaseDate           string             `json:"usReleaseDate"`
 }
 
-type QuadrantStruct struct {
-	UID string `json:"uid"`
-	Name string `json:"name"`
-	AstronomicalObjectType string `json:"astronomicalObjectType"`
-	Location LocationStruct `json:"location"`
-
+type CharacterSpeciesStruct struct {
+	UID         string `json:"uid"`
+	Name        string `json:"name"`
+	Numerator   int64  `json:"numerator"`
+	Denominator int64  `json:"denominator"`
 }
 
-type SpeciesStruct struct {
-	UID string `json:"uid"`
-	Name string `json:"name"`
-	HomeWorld HomeWorldStruct `json:"homeWorldStruct"`
-	Quadrant QuadrantStruct `json:"quadrant"`
-	ExtinctSpecies bool `json:"extinctSpecies"`
-	WarpCapableSpecies bool `json:"warpCapableSpecies"`
-	ExtraGalacticSpecies bool `json:"extraGalacticSpecies"`
-	HumanoidSpecies bool `json:"humanoidSpecies"`
-	ReptilianSpecies bool `json:"reptilianSpecies"`
-	NonCorporealSpecies bool `json:"nonCorporealSpecies"`
-	ShapeshiftingSpecies bool `json:"shapeshiftingSpecies"`
-	SpaceborneSpecies bool `json:"spaceborneSpecies"`
-	TelepathicSpecies bool `json:"telepathicSpecies"`
-	TransDimensionalSpecies bool `json:"transDimensionalSpecies"`
-	UnnamedSpecies bool `json:"unnamedSpecies"`
-	AlternateReality bool `json:"alternateReality"`
-	Characters []CharacterStruct `json:"characters"`
+type TitlesStruct struct {
+	UID            string `json:"uid"`
+	Name           string `json:"name"`
+	MilitaryRank   bool   `json:"militaryRank"`
+	FleetRank      bool   `json:"fleetRank"`
+	ReligiousTitle bool   `json:"religiousTitle"`
+	Position       bool   `json:"position"`
+	Mirror         bool   `json:"mirror"`
 }
 
-//TODO: Create Character Structs
+type Character2Struct struct {
+	UID                    string                   `json:"uid"`
+	Name                   string                   `json:"name"`
+	Gender                 string                   `json:"gender"`
+	YearOfBirth            int64                    `json:"yearOfBirth"`
+	MonthOfBirth           int64                    `json:"monthOfBirth"`
+	DayOfBirth             int64                    `json:"dayOfBirth"`
+	PlaceOfBirth           string                   `json:"placeOfBirth"`
+	YearOfDeath            int64                    `json:"yearOfDeath"`
+	MonthOfDeath           int64                    `json:"monthOfDeath"`
+	DayOfDeath             int64                    `json:"dayOfDeath"`
+	PlaceOfDeath           string                   `json:"placeOfDeath"`
+	Height                 float32                  `json:"height"`
+	Weight                 float32                  `json:"weight"`
+	Deceased               bool                     `json:"deceased"`
+	BloodType              string                   `json:"bloodType"`
+	MaritalStatus          string                   `json:"string"`
+	SerialNumber           string                   `json:"serialNumber"`
+	HologramActivationDate int32                    `json:"hologramActivationDate"`
+	HologramStatus         string                   `json:"hologramStatus"`
+	HologramDateStatus     string                   `json:"hologramDateStatus"`
+	Hologram               bool                     `json:"hologram"`
+	FictionalCharacter     bool                     `json:"fictionalCharacter"`
+	Mirror                 bool                     `json:"mirror"`
+	AlternateReality       bool                     `json:"alternateReality"`
+	Performers             []PerformerStruct        `json:"performers"`
+	Episodes               []EpisodeStruct          `json:"episodes"`
+	Movies                 []MoviesStruct           `json:"movies"`
+	CharacterSpecies       []CharacterSpeciesStruct `json:"characterSpecies"`
+	CharacterRelations     []string                 `json:"characterRelations"`
+	Titles                 []TitlesStruct           `json:"titles"`
+	Organizations          []string                 `json:"organizations"`
+}
+
+type CharacterFullStruct struct {
+	Character Character2Struct `json:"character"`
+}
 
 func main() {
-
 
 	//Command line arguments
 
@@ -117,7 +207,6 @@ func main() {
 
 	//TODO: Translate to Klingon
 
-
 	//Call to Character Endpoint
 
 	//FIXME: Change to arguments
@@ -125,6 +214,7 @@ func main() {
 	data.Set("title", "Uhura")
 	data.Add("name", "Uhura")
 
+	// Call API to search character by name
 	req, err := http.NewRequest("POST", characterSearchUrl, bytes.NewBufferString(data.Encode()))
 	if err != nil {
 		log.Fatal("NewRequest: ", err)
@@ -142,56 +232,55 @@ func main() {
 
 	var inputJson PageStruct
 
-
 	if err := json.NewDecoder(resp.Body).Decode(&inputJson); err != nil {
 		log.Println(err)
 	}
 
-	var elements int64
-	elements = inputJson.Page.NumberOfElements
+	// Check API result
+	if resp.StatusCode == 200 {
 
-	if elements > 0 {
-		fmt.Println(inputJson.Characters[0].UID)
+		var elements int64
+		elements = inputJson.Page.NumberOfElements
 
-		// Make another call to get Character Details
+		// Check if character exists
+		if elements > 0 {
+			// Character key is inputJson.Characters[0].UID
 
-		//TODO: Make call to get character details
+			// Make another call to get Character Details
 
+			newUrl := characterUrl+inputJson.Characters[0].UID
 
-		// Make another call to get Species
-		//FIXME: Change url
-		req3, err := http.NewRequest("GET", specieUrl + inputJson.Characters[0].UID, nil)
-		if err != nil {
-			log.Fatal("NewRequest: ", err)
-			return
+			req2, err := http.NewRequest("GET", newUrl, nil)
+			if err != nil {
+				log.Fatal("NewRequest: ", err)
+				return
+			}
+			client2 := &http.Client{}
+
+			resp2, err := client2.Do(req2)
+			if err != nil {
+				log.Fatal("Do: ", err)
+				return
+			}
+
+			defer resp2.Body.Close()
+
+			var characterJson CharacterFullStruct
+
+			if err := json.NewDecoder(resp2.Body).Decode(&characterJson); err != nil {
+				log.Println(err)
+			}
+
+			//Print Species
+			if resp.StatusCode == 200 {
+				fmt.Println(characterJson.Character.CharacterSpecies[0].Name)
+			} else {
+				fmt.Println("Error retrieving character species")
+			}
+		} else {
+			fmt.Println("Character not found")
 		}
-		client3 := &http.Client{}
-
-		resp3, err := client3.Do(req3)
-		if err != nil {
-			log.Fatal("Do: ", err)
-			return
-		}
-
-		defer resp3.Body.Close()
-
-		var speciesJson SpeciesStruct
-
-
-		if err := json.NewDecoder(resp3.Body).Decode(&speciesJson); err != nil {
-			log.Println(err)
-		}
-
-		//TODO: Erase this
-		fmt.Println(resp.Status)
-
-		fmt.Println(speciesJson)
-		fmt.Println(speciesJson.Name)
-
-
-
 	} else {
-		fmt.Println("Character not found")
+		fmt.Println("Error searching for character")
 	}
-
 }
